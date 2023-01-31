f0de496e9f42ce3352a72249253d9f9461709ab4
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-01-31 19:12:29 +0530

    Wecome to Data Structure program using Generics

---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data structure program using generics");
+        LinkedList<Integer> linkedList = new LinkedList();
+        linkedList.add(70);
+        linkedList.display();
+        System.out.println();
+    }
+}
