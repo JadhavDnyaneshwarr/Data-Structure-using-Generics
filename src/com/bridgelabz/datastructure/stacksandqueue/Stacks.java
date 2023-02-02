53055a341191756206d5f0063ded3bc7aa740b2a
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2023-02-02 19:56:02 +0530

    Added: creating a Stacks

--- src/com/bridgelabz/datastructure/stacksandqueue/Stacks.java
+++ src/com/bridgelabz/datastructure/stacksandqueue/Stacks.java
@@ -1,12 +1,17 @@
 package com.bridgelabz.datastructure.stacksandqueue;
 
 import com.bridgelabz.datastructure.linkedlist.LinkedList;
-import com.bridgelabz.datastructure.linkedlist.Node;
 
 public class Stacks<T> {
     LinkedList<T> list;
     Stacks() {list = new LinkedList<>();}
 
+    public void add (T data){
+        list.add(data);
+    }
+    public void append (T data){
+        list.append(data);
+    }
     public void display() {
         list.display();
     }
